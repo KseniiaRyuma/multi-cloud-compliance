dcd92c56cf4c090b0cf9843041b0db5912bd18d1
KseniiaRyuma <kseniya0213@gmail.com>
2022-04-04 23:15:07 -0400

    connections.tf

--- connections.tf
+++ connections.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+
+    
+  }
+}
+
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
